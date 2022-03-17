7a4f999a1d97c0acbd0c89dfbbbb6a1ce2f10b23
Allisonfreittass <100173728+Allisonfreittass@users.noreply.github.com>
2022-03-17 14:05:45 -0300

    Update tarefa03.php

--- tarefa03.php
+++ tarefa03.php
@@ -1,21 +1,21 @@
 <?php
 
 class produto{
+
  public $descricao;
  public $estoque;
  public $preco;
 
- public function aumentarEstoque ($valor){
+ public function aumentarEstoque ($valor) {
      $this->estoque += $valor;
  }
 
   public function diminuirEstoque ($valor){
       $this->estoque -= $valor;
   } 
- public function 
-  reajustarPreco($percentual){
-      $valoreajuste = ($percentual*$this->preco) / 100;
-      $this->preco += $valorreajuste
+
+ public function reajustarPreco($percentual){
+    $this->preco += ($percentual*$this->preco)/100;
   }
 }
 
@@ -25,10 +25,13 @@ $celular->descricao = "novo processador";
 $celular->estoque = 1000;
 $celular->preco = 1500;
 
-echo "O $celular->decricao tem apenas $celular->estoque unidades em nossa loja, com o incrível valor $celular->preco!!";
+echo 'O estoque de '. $celular->descricao. ' é '. $celular->estoque. '<br>';
+echo 'O preço de '.  $celular->descricao. ' é '. $celular->preco;
+echo '<br>';
 
-$celular->aumentarEstoque(15);
-$celular->diminuirEstoque(10);
+$celular->aumentarEstoque(10);
+$celular->diminuirEstoque(5);
 $celular->reajustarPreco(50);
 
-echo "O $celular->descricao tem estoque $celular->estoque com o preco unitário de R$ $P$celular->preco";
+echo 'o estoque de '. $celular->descricao . ' é '. $celular->estoque. '<br>';
+echo 'O preço de '. $celular->descricao . ' é '. $celular->preco;
